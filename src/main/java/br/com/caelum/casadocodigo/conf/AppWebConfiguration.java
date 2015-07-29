74688eb940512b29cd8bfaa5723f1f8e62262b4e
Rafael Rollo <rafaelrollo92@gmail.com>
2015-07-29 10:12:21 -0300

    Init

---
+++ src/main/java/br/com/caelum/casadocodigo/conf/AppWebConfiguration.java
@@ -0,0 +1,22 @@
+package br.com.caelum.casadocodigo.conf;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+@Configuration
+@EnableWebMvc
+@ComponentScan(basePackages = "br.com.caelum.casadocodigo")
+public class AppWebConfiguration extends WebMvcConfigurerAdapter
+{
+
+   @Override
+   public void configureDefaultServletHandling(DefaultServletHandlerConfigurer configurer)
+   {
+      configurer.enable();
+   }
+
+}
