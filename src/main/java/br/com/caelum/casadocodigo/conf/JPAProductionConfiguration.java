903ff110b80fc8ac844c236611be34230fb93e5d
Cassio Almeida <cassio.salmeida@gmail.com>
2015-08-10 17:46:51 -0300

    Configuracoes para heroku

--- src/main/java/br/com/caelum/casadocodigo/conf/JPAProductionConfiguration.java
+++ src/main/java/br/com/caelum/casadocodigo/conf/JPAProductionConfiguration.java
@@ -11,14 +11,13 @@ import org.springframework.context.annotation.Profile;
 import org.springframework.core.env.Environment;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
 	
-
+@Profile("prod")
 public class JPAProductionConfiguration {
 
 	@Autowired
 	private Environment environment;
 	
 	@Bean
-	@Profile("prod")
 	public DataSource dataSource(){
 		DriverManagerDataSource dataSource = new DriverManagerDataSource();
 		dataSource.setDriverClassName("org.postgresql.Driver");
