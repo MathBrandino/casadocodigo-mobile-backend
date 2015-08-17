ec596b00bd0a8be2ddfca60e737e2da9d033ea80
Rafael Rollo <rafaelrollo92@gmail.com>
2015-08-17 17:23:14 -0300

    Excluindo classe EntryPoint, atualizando EntryPointController

--- src/main/java/br/com/caelum/casadocodigo/controllers/EntryPointResourceController.java
+++ src/main/java/br/com/caelum/casadocodigo/controllers/EntryPointResourceController.java
@@ -3,13 +3,12 @@ package br.com.caelum.casadocodigo.controllers;
 import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.hateoas.Link;
+import org.springframework.hateoas.ResourceSupport;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-
-import br.com.caelum.casadocodigo.resources.EntryPoint;
 
 @Controller
 @RequestMapping("/")
@@ -18,18 +17,18 @@ public class EntryPointResourceController {
 	@RequestMapping(
 			method = RequestMethod.GET,
 			produces = MediaType.APPLICATION_JSON_VALUE )
-	public HttpEntity<EntryPoint> options(HttpServletRequest req) {
+	public HttpEntity<ResourceSupport> options(HttpServletRequest req) {
 		
 		String url = req.getRequestURL().toString();
 		Link listarLivros = new Link(url + "livros", "listarLivros");
 		Link listarCompras = new Link(url + "compras", "listarCompras");
-		Link registrarCompras = new Link(url + "compras", "registrarCompras");
+		Link registrarCompras = new Link(url + "compras", "registrarCompra");
 		
-		EntryPoint resource = new EntryPoint();
+		ResourceSupport resource = new ResourceSupport();
 		resource.add(listarLivros);
 		resource.add(listarCompras);
 		resource.add(registrarCompras);
 		
-		return new HttpEntity<EntryPoint>(resource);
+		return new HttpEntity<ResourceSupport>(resource);
 	}
 }
--- src/main/java/br/com/caelum/casadocodigo/resources/EntryPoint.java
+++
@@ -1,7 +0,0 @@
-package br.com.caelum.casadocodigo.resources;
-
-import org.springframework.hateoas.ResourceSupport;
-
-public class EntryPoint extends ResourceSupport {
-
-}
